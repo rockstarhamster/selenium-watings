8ca6a5f60c1a0e4c44db11a9c49f9d96796d0400
amidinitsin-zap <a.midinitsyn@zaptain.dev>
2021-08-30 23:16:40 +0300

    webinar final result

--- src/test/java/ru/rockstarhamster/selenium/waitings/SeleniumExpectationsTest.java
+++ src/test/java/ru/rockstarhamster/selenium/waitings/SeleniumExpectationsTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -12,18 +11,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class SeleniumExpectationsTest {
 
-    private static final Duration ACCEPT_COOKIE_WAIT_TIMEOUT = Duration.ofSeconds(5);
-    private static final String ACCEPT_COOKIE_BUTTON_ID = "onetrust-accept-btn-handler";
-    private static final String HM_HOME_PAGE_URL = "https://www2.hm.com/ru_ru/index.html";
-    private static final String HM_COOKIE_DOMAIN_NAME = ".hm.com";
-
-    private static final String DOGMA_PAGE_URL = "https://1dogma-krasnodar.ru/";
-    private static final Duration ISSUE_POPUP_TIMEOUT = Duration.ofSeconds(60);
-    private static final String POP_UP_ISSUE_MODEL_CLASS = "i-lh-main";
+    private static final String FROM_KRD_TO_MSK_TICKET_SEARCH_URL = "https://www.aviasales.ru/search/KRR3009MOW15101?request_source=search_form&payment_method=all";
+    private static final String SUBSCRIPTION_BUTTON_CLASSNAME = "direction-subscriptions__btn-subs";
+    private static final String PREDICTION_FIRST_COLUMN_CLASSNAME = "prediction-header__left-col";
 
     private static WebDriver webDriver;
 
@@ -31,29 +25,21 @@ public class SeleniumExpectationsTest {
     public static void setUpWebDriver() {
         System.setProperty("webdriver.chrome.driver", "/home/alex/IdeaProjects/selenium-watings/src/test/resources/chromedriver");
         webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        webDriver.manage().window().maximize();
     }
 
     @Test
-    public void accept_hm_cookie() {
-        webDriver.get(HM_HOME_PAGE_URL);
-        final var driverWait = new WebDriverWait(webDriver, ACCEPT_COOKIE_WAIT_TIMEOUT);
-        driverWait.until(ExpectedConditions.elementToBeClickable(By.id(ACCEPT_COOKIE_BUTTON_ID)));
-        webDriver.findElement(By.id(ACCEPT_COOKIE_BUTTON_ID)).click();
-        final var domain = webDriver.manage().getCookies().stream()
-                .map(Cookie::getDomain)
-                .findFirst();
-        assertTrue(domain.isPresent());
-        assertEquals(HM_COOKIE_DOMAIN_NAME, domain.get());
+    public void subscription_button_appears() {
+        webDriver.get(FROM_KRD_TO_MSK_TICKET_SEARCH_URL);
+        assertDoesNotThrow(() -> webDriver.findElement(By.className(SUBSCRIPTION_BUTTON_CLASSNAME)));
     }
 
     @Test
-    public void check_that_user_gets_issue_popup() {
-        webDriver.get(DOGMA_PAGE_URL);
-        final var driverWait = new WebDriverWait(webDriver, ISSUE_POPUP_TIMEOUT);
-        final var webElement = driverWait.until(
-                ExpectedConditions.elementToBeClickable(By.className(POP_UP_ISSUE_MODEL_CLASS))
-        );
-        assertNotNull(webElement);
+    public void prediction_bar_appears() {
+        webDriver.get(FROM_KRD_TO_MSK_TICKET_SEARCH_URL);
+        final var wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
+        assertDoesNotThrow(() -> wait.until(ExpectedConditions.presenceOfElementLocated(By.className(PREDICTION_FIRST_COLUMN_CLASSNAME))));
     }
 
     @AfterAll
